507776dba77b7b11775a984fe51ba7cb8187fdac
unknown <duynguyen0428@gmail.com>
2018-05-17 00:29:49 -0700

    add error hanndler

--- Startup.cs
+++ Startup.cs
@@ -14,6 +14,9 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
 using System.Text;
+using System.Net;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Http;
 
 namespace web_api
 {
@@ -58,6 +61,18 @@ namespace web_api
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+            }
+            else{
+                app.UseExceptionHandler(builder => {
+                   builder.Run( async context => {
+                       context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+
+                       var error = context.Features.Get<IExceptionHandlerFeature>();
+                       if(error!=null){
+                           await context.Response.WriteAsync(error.Error.Message);
+                       }
+                   });
+                });
             }
             // use CORS
             app.UseCors( x => x.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin().AllowCredentials());
